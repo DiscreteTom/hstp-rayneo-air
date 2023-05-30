7476b93e97fcbb4a3621741fc37c5529b5c95175
DiscreteTom <discrete_tom@outlook.com>
2023-05-30 19:27:54 +0800

    refactor!: no param, return pointer

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,21 @@ extern "C" {
   fn GetArSensor() -> *mut u8;
 }
 
+static mut POS_DATA: PositionData = PositionData {
+  enabled: false,
+  x: 0.0,
+  y: 0.0,
+  z: 0.0,
+};
+
+static mut ROT_DATA: RotationData = RotationData {
+  enabled: false,
+  x: 0.0,
+  y: 0.0,
+  z: 0.0,
+  w: 0.0,
+};
+
 pub fn init() {
   unsafe { XRSDK_Init() }
 }
@@ -17,18 +32,20 @@ pub fn shutdown() {
   unsafe { XRSDK_Shutdown() }
 }
 
-pub fn get_position(data: &mut PositionData) {
-  data.enabled = false;
+pub unsafe fn get_position() -> *mut PositionData {
+  POS_DATA.enabled = false;
+  &mut POS_DATA
 }
 
-pub fn get_rotation(data: &mut RotationData) {
-  data.enabled = false;
+pub unsafe fn get_rotation() -> *mut RotationData {
+  ROT_DATA.enabled = false;
   unsafe {
     let p = GetArSensor();
     // read float from p+44
-    data.x = *(p.add(44)) as f32;
-    data.y = *(p.add(48)) as f32;
-    data.z = *(p.add(52)) as f32;
-    data.w = *(p.add(56)) as f32;
+    ROT_DATA.x = *(p.add(44)) as f32;
+    ROT_DATA.y = *(p.add(48)) as f32;
+    ROT_DATA.z = *(p.add(52)) as f32;
+    ROT_DATA.w = *(p.add(56)) as f32;
   }
+  &mut ROT_DATA
 }
